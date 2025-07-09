cae9374c1d0ece084ac90ca589f535dd75249dc3
Brandon Benton <brandon.benton.5255@gmail.com>
2025-07-08 19:29:43 -0500

    Github link for projects

--- projects/models.py
+++ projects/models.py
@@ -5,3 +5,4 @@ class Project(models.Model):
     description = models.TextField()
     technology = models.CharField(max_length=20)
     image = models.FileField(upload_to="project_images/", blank=True)
+    github_link = models.TextField()
