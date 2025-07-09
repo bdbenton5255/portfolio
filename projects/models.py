f0e9ab7a42263afeda6baa71cf39eaab2fcb6512
Brandon Benton <brandon.benton.5255@gmail.com>
2025-07-08 19:50:05 -0500

    model charfield extension

--- projects/models.py
+++ projects/models.py
@@ -3,6 +3,6 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    technology = models.CharField(max_length=20)
+    technology = models.CharField(max_length=30)
     image = models.FileField(upload_to="project_images/", blank=True)
     github_link = models.TextField()
